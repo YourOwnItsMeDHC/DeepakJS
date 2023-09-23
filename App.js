365509bd9754b34e5354137063815894393e70a9
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2023-09-23 11:36:45 +0530

    Parameters of React.createElement()

---
+++ App.js
@@ -0,0 +1,19 @@
+//React.createElement(elemName, props, content / what_all_things_I_wanna_put)
+//props {} , will store attributes of elemName
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello Deepak using ReactJs"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(heading); //object
+
+//Each and every element in ReactJs is an Object
+// Third argument in React.createElement() is "children" i.e. what these particular element should consist
+//Putting these newly created h1 tag inside "root"
+
+//render h1 inside root
+//render ==> to give/provide something
+root.render(heading);
