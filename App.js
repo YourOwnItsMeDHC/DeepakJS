35cf70cbb3e28f9da63a72ae8d8afbc864f545ed
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2023-10-04 09:10:56 +0530

    Functional Component

--- App.js
+++ App.js
@@ -66,6 +66,8 @@ const cont = React.createElement(
 //JSX - It's getting transpiled, before it reaches the JSX
 // JSX ==> Babel trasnpiles it to ==> React.createElement ==> React element - JS object ==> HTMLElement(render)
 // In JSX, attributes should be in camel case
+
+// ******************************* React Element **********************************
 const jsxHeading = (
   <h1 id="heading" className="head">
     I am JSX heading
@@ -73,5 +75,49 @@ const jsxHeading = (
 );
 console.log(jsxHeading); //Object
 
+// ******************************* Functional Component **********************************
+// Functional Component ==> A function which returns jsx code or return react element
+// or return bunch of react element
+/*
+Arrow Function :
+ const fn => () = true;
+
+ const fn2 => () = {
+  return true
+
+  Above, both syntax is Ok
+ */
+
+// Component Composition ==> Component inside another component
+//Functional component using Arrow function
+const HeaderComponent = () => {
+  return (
+    <div>
+      {/* Using react element inside component */}
+      {cont}
+      <h1>I am head 1 from header component</h1>
+      <h2>I am head 2 from header component</h2>
+    </div>
+  );
+};
+
+//Without "return" keyword is also OK
+const HeaderComponent2 = () => (
+  <div>
+    <h1>I am head 1 from header component</h1>
+    <h2>I am head 2 from header component</h2>
+  </div>
+);
+
+//Functional component using normal function
+const HeaderComponent3 = function () {
+  return (
+    <div>
+      <h1>I am head 1 from header component</h1>
+      <h2>I am head 2 from header component</h2>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+
+root.render(<HeaderComponent />);
