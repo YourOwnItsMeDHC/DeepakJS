c6c3b622bcbf7c2842ccf5ddeae2f12ac4707c56
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2023-10-06 09:07:29 +0530

    Restaurant Card - Hardcoded

--- App.js
+++ App.js
@@ -39,8 +39,63 @@ const Header = () => {
   );
 };
 
+// Object of Style, which we will pass as CSS inline styling
+const styleCard = {
+  backgroundColor: "#f0f0f0",
+};
+
+const RestaurantCard = () => {
+  return (
+    // <div className="res-card" style={styleCard}>
+    // Instead of passing styleCard object, we can direvtly pass object of styling attributes
+    <div
+      className="res-card"
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bz9zkh2aqywjhpankb07"
+      ></img>
+      <h3>Deepak's Food Shop</h3>
+      <h4>France, American, Indian</h4>
+      <h4>4.4 stars</h4>
+      <h4>25 minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
 const AppLayout = () => {
-  return <Header />;
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
