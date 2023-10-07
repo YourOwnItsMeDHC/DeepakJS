58c5ca143e326e37c2137e9929895f37db152c84
YourOwnItsMeDHC <deepakchourasiya7980@gmail.com>
2023-10-07 22:04:37 +0530

    Fetching dynamic data for restaurant card

--- App.js
+++ App.js
@@ -44,7 +44,8 @@ const styleCard = {
   backgroundColor: "#f0f0f0",
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const { resData } = props;
   return (
     // <div className="res-card" style={styleCard}>
     // Instead of passing styleCard object, we can direvtly pass object of styling attributes
@@ -57,14 +58,143 @@ const RestaurantCard = () => {
       <img
         className="res-logo"
         alt="res-logo"
-        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/bz9zkh2aqywjhpankb07"
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          resData.data.cloudinaryImageId
+        }
       ></img>
-      <h3>Deepak's Food Shop</h3>
-      <h4>France, American, Indian</h4>
-      <h4>4.4 stars</h4>
-      <h4>25 minutes</h4>
+      <h3>{resData.data.name}</h3>
+      <h4>{resData.data.cuisines.join(", ")}</h4>
+      <h4>{resData.data.avgRating}</h4>
+      <h4>Rs. {resData.data.costForTwo / 100}</h4>
+      <h4>{resData.data.deliveryTime} minutes</h4>
     </div>
   );
+};
+
+const resObj = {
+  type: "restaurant",
+  data: {
+    type: "F",
+    id: "334475",
+    name: "KFC",
+    uuid: "eaed0e3b-7c0e-4367-8f59-f41d309fb93a",
+    city: "1",
+    area: "BTM Layout",
+    totalRatingsString: "500+ ratings",
+    cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+    cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+    tags: [],
+    costForTwo: 40000,
+    costForTwoString: "₹400 FOR TWO",
+    deliveryTime: 36,
+    minDeliveryTime: 36,
+    maxDeliveryTime: 36,
+    slaString: "36 MINS",
+    lastMileTravel: 3.5,
+    slugs: {
+      restaurant: "kfc-btm-layout-btm",
+      city: "bangalore",
+    },
+    cityState: "1",
+    address: "KFC restaurants, 942,SV Tower, 16th Main, BTM 2nd Stage ,560076",
+    locality: "2nd Stage",
+    parentId: 547,
+    unserviceable: false,
+    veg: false,
+    select: false,
+    favorite: false,
+    tradeCampaignHeaders: [],
+    aggregatedDiscountInfo: {
+      header: "FREE DELIVERY",
+      shortDescriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      subHeader: "",
+      headerType: 0,
+      superFreedel: "",
+    },
+    aggregatedDiscountInfoV2: {
+      header: "",
+      shortDescriptionList: [
+        {
+          meta: "Free Delivery",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "FREE DELIVERY",
+          discountType: "FREE_DELIVERY",
+          operationType: "RESTAURANT",
+        },
+      ],
+      subHeader: "",
+      headerType: 0,
+      superFreedel: "",
+    },
+    ribbon: [
+      {
+        type: "PROMOTED",
+      },
+    ],
+    chain: [],
+    feeDetails: {
+      fees: [],
+      totalFees: 0,
+      message: "",
+      title: "",
+      amount: "",
+      icon: "",
+    },
+    availability: {
+      opened: true,
+      nextOpenMessage: "",
+      nextCloseMessage: "",
+    },
+    longDistanceEnabled: 0,
+    rainMode: "NONE",
+    thirdPartyAddress: false,
+    thirdPartyVendor: "",
+    adTrackingID: "cid=6109309~p=1~eid=00000186-a341-249f-05e6-09c500910178",
+    badges: {
+      imageBased: [],
+      textBased: [],
+      textExtendedBadges: [],
+    },
+    lastMileTravelString: "3.5 kms",
+    hasSurge: false,
+    sla: {
+      restaurantId: "334475",
+      deliveryTime: 36,
+      minDeliveryTime: 36,
+      maxDeliveryTime: 36,
+      lastMileTravel: 3.5,
+      lastMileDistance: 0,
+      serviceability: "SERVICEABLE",
+      rainMode: "NONE",
+      longDistance: "NOT_LONG_DISTANCE",
+      preferentialService: false,
+      iconType: "EMPTY",
+    },
+    promoted: true,
+    avgRating: "3.8",
+    totalRatings: 500,
+    new: false,
+  },
+  subtype: "basic",
 };
 
 const Body = () => {
@@ -72,18 +202,7 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard resData={resObj} />
       </div>
     </div>
   );
